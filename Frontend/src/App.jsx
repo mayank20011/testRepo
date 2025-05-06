99a05e063a312d055fe063cc93ff4c52c38eef71
mayank20011 <mayank.behl.mb1@gmail.com>
2025-05-06 15:15:43 +0530

    changed home to Home in app.jsx of frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SpecificBlog from "./pages/SpecificBlog";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import ReadFullBlog from "./pages/readFullBlog";
 import Dashboard from "./pages/Dashboard";
 
