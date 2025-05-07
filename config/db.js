0dc4202c5bfe665f05ce4339f892bfa5551b6264
mayank20011 <mayank.behl.mb1@gmail.com>
2025-05-07 15:17:48 +0530

    made db connection a promise and server start only after db connection

--- config/db.js
+++ config/db.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config({path:"./config/config.env"});
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then((res) => {
-    console.log("Db Connected SuccessFully");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const connectDb = ()=>{
+  return mongoose.connect(process.env.MONGO_URI);
+}
+
+export default connectDb;
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then((res) => {
+//     console.log("Db Connected SuccessFully");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRouter from "./routes/users.js";
-import "./config/db.js";
 import blogsRouter from "./routes/blog.js";
 import cors from "cors";
 import commentsRouter from "./routes/comment.js";
+import connectDb from "./config/db.js";
 
 // dotenv path config
 dotenv.config({ path: "./config.env" });
@@ -17,11 +17,13 @@ const server = express();
 // body parser middleware
 server.use(express.json());
 // cors
-server.use(cors({
-  origin :"*",
-  methods:["GET","PUT","POST","PATCH","DELETE"],
-  allowedHeaders: ["Content-Type"]
-}));
+server.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 // to check if server is working or not
 server.get("/", (req, res) => {
@@ -32,6 +34,17 @@ server.use("/api/v1/user", userRouter);
 server.use("/api/v1/blog", blogsRouter);
 server.use("/api/v1/comment", commentsRouter);
 
-server.listen(PORT, () => {
-  console.log(`Server Running on Port: ${PORT}`);
-});
+connectDb()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server Running on Port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed To connect to mongodb", err);
+    process.exit(1);
+  });
+
+// server.listen(PORT, () => {
+//   console.log(`Server Running on Port: ${PORT}`);
+// });
